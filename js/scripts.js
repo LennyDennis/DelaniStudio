c2c32dff871fd396fff0a3c85f1a7294dcf4c4fd
LennyDennis <lennydennis@gmail.com>
2019-12-08 16:37:47 +0300

    validates the form and also adds an alert on submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  //toggle at design,development and product mgmt
   $(".toggle1").click(function(){
     $(".image1").toggle();
     $(".design").toggle();
@@ -11,4 +12,42 @@ $(document).ready(function(){
     $(".image3").toggle();
     $(".product_mgmt").toggle();
   });
+
+  //display white box on hover
+  // $(".project").hover(function(){
+  //   $(this).animate({opacity:'1'});
+  // },
+  // function(){
+  //   $(this).animate({opacity:'0'});
+  // });
+
+  $(".button").click(function(event){
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    if(name == ""){
+      alert("Please fill the name field");
+      name.focus();
+      return false;
+    }
+    if(email == ""){
+      alert("Please fill the email field");
+      email.focus();
+      return false;
+    }
+    if(message == ""){
+      alert("Please fill the message field");
+      message.focus();
+      return false;
+    }
+    alert(name +", we have received your message. Thank you for contacting us.");
+  });
+  $("button").click(function(event){
+    $('form').each(function(){
+      this.reset();
+    });
+  });
+
 });
+
